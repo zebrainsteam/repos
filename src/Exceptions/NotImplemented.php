51b2c8c6930dbbb7e8fc8b018bb262849b55a97b
Artem Prozorov <artem.g.prozorov@gmail.com>
2020-06-15 20:02:51 +0300

    Added exceptions

---
+++ src/Exceptions/NotImplemented.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Prozorov\Repositories\Exceptions;
+
+use RuntimeException;
+
+class NotImplemented extends RuntimeException
+{
+
+}
---
+++ src/Exceptions/RepositoryException.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Prozorov\Repositories\Exceptions;
+
+use InvalidArgumentException;
+
+class RepositoryException extends InvalidArgumentException
+{
+    /**
+     * @var string $repositoryCode
+     */
+    protected $repositoryCode;
+
+    /**
+     * setRepositoryCode.
+     *
+     * @access	public
+     * @param	string	$code	
+     * @return	void
+     */
+    public function setRepositoryCode(string $code): void
+    {
+        $this->repositoryCode = $code;
+    }
+
+    /**
+     * Returns repository code.
+     *
+     * @access	public
+     * @return	string
+     */
+    public function getRepositoryCode(): string
+    {
+        return (string) $this->repositoryCode;
+    }
+}
