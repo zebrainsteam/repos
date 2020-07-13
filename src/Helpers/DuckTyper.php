e64edf55f6ad215acb0920cca7eed4579936f0e3
Artem Prozorov <artem.g.prozorov@gmail.com>
2020-07-13 03:10:07 +0300

    Added DuckTyper helper

---
+++ src/Helpers/DuckTyper.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Prozorov\Repositories\Helpers;
+
+use Prozorov\Repositories\Exceptions\InvalidDataType;
+
+class DuckTyper
+{
+    /**
+     * getId.
+     *
+     * @access	public static
+     * @param	object	$potentialyHasId
+     * @param	string	$idField Default: 'id'
+     * @throws  InvalidDataType
+     * @return	int
+     */
+    public static function getId(object $potentialyHasId, string $idField = 'id'): int
+    {
+        if (property_exists($potentialyHasId, $idField) && is_int($potentialyHasId->$idField)) {
+            return $potentialyHasId->$idField;
+        }
+
+        $getterFunction = 'get' . ucfirst($idField);
+
+        if (method_exists($potentialyHasId, $getterFunction) && is_int($potentialyHasId->$getterFunction())) {
+            return $potentialyHasId->$getterFunction();
+        }
+
+        throw new InvalidDataType('Id could not be found in object');
+    }
+}
